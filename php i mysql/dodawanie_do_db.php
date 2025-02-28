9fbbb2492e52a90791f0239ea43203a8d0fdd0b8
XMASTEr <54854992+XMASTEr1432@users.noreply.github.com>
2025-02-28 11:08:32 +0100

    Add files via upload

--- php i mysql/dodawanie_do_db.php
+++ php i mysql/dodawanie_do_db.php
@@ -7,11 +7,11 @@
 </head>
 <body>
     <?php 
-        if (!isset($_GET['id']) || !isset($_GET['imie']) || !isset($_GET['nazwisko']) || !isset($_GET('rok')) || !isset($_GET['miasto;'])) {
+        if (!isset($_GET['id']) || !isset($_GET['imie']) || !isset($_GET['nazwisko']) || !isset($_GET('rok')) || !isset($_GET['miasto'])) {
             exit('Brak danych. </body></html>');
         }
-        if ($_GET['id'] == '' || $_GET('nazwisko') == '' || $_GET['nazwisko'] =='' || $_GET['rok'] =='' || $_GET['miasto'] == '')  {
-            exit("Brkaaaaaaak danych.");
+        if ($_GET['id'] == '' || $_GET('imie') == '' || $_GET['nazwisko'] =='' || $_GET['rok'] =='' || $_GET['miasto'] == '')  {
+            exit("Brkaaaaaaak danych. </body></html>");
         }
 
         $id = $_GET['id'];
@@ -20,7 +20,36 @@
         $rok = $_GET['rok'];
         $miasto = $_GET['miasto'];
 
+        if (!$db_lnk = @mysqli_connect("localhost", "root", "")) {
+            echo('Wystąpił błąd próby połączenia z serwerem MySQLi...<br>');
+            echo("</body></html>");
+            exit;
+        }
 
+        if (!@mysqli_select_db($db_lnk, "baza_testowa")) {
+            @mysqli_close();
+            echo("Wystąpił błąd podczas wyboru bazy danych: baza_testowa <br>");
+            echo("</body></html>");
+            exit;
+        }
+
+        $query = "INSERT INTO osobaa VALUES('$id', '$imie', '$nazwisko', '$rok', '$miasto')";
+
+        if (!$result = mysqli_query($db_lnk, $query)) {
+            @mysqli_close($db_lnk);
+            echo("Wystąpił błąd: nieprawidłowe zapytanie...<br>");
+            echo('</body></html>');
+            exit;
+        }
+
+        $rowsNo = mysqli_affected_rows($db_lnk);
+
+        echo("Liczba dodanych rekordów: $rowsNo <br>");
+        if(!mysqli_close($db_lnk))
+        {
+            echo("Wystąpił błąd podczas zamykania połączenia z serwerem MySQLi...<br>");
+            exit;
+        }
     ?>
 </body>
 </html>
